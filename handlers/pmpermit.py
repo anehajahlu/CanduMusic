a0970b61e070d2bb61edf68d40c30116f98d1ca4
Geez <79180058+vckyou@users.noreply.github.com>
2021-04-22 08:54:03 +0700

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -7,3 +7,4 @@ from pyrogram.types import Chat, Message, User
 async def pmPermit(client: USER, message: Message):
   await USER.send_message(message.chat.id,"Halo, Saya adalah **Layanan Asisten Musik dari @CanduMusicBot.\n -Silahkan Ketik /Start Ke @CanduMusicBot**\n\n ❗️ **Rules :**\n   - Jangan spam Lagu biar ga error \n\n 👉 **KIRIM LINK UNDANGAN GRUP ATAU NAMA PENGGUNA JIKA USERBOT TIDAK DAPAT BERGABUNG DENGAN GRUP ANDA.**\n\n ⛑ **Group Support :** @VcgSupportGroup - **Owner** @VckyouuBitch\n\n")
   return                        
+
