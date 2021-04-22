acc2c60378fbf3bd4114419a761f4261283c35bd
Geez <79180058+vckyou@users.noreply.github.com>
2021-04-22 08:08:32 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 admins
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "240"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
