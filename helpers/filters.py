27f9e90bafb3158599b838c10931751f1c23f2c3
Geez <79180058+vckyou@users.noreply.github.com>
2021-04-22 08:45:37 +0700

    Update filters.py

--- helpers/filters.py
+++ helpers/filters.py
@@ -11,3 +11,4 @@ other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filt
 def command(commands: Union[str, List[str]]):
     return filters.command(commands, COMMAND_PREFIXES)
 
+
